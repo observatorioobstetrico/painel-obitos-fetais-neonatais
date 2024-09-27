7c8399d9c8e35bfb5bcefe050072f748a6bafd78
Rafael Herzog <rafasantanah21@gmail.com>
2024-09-27 14:59:44 -0300

    atualização do mês de setembro de 2024

--- R/baixa_dados.R
+++ R/baixa_dados.R
@@ -3,14 +3,18 @@ library(janitor)
 library(readr)
 library(microdatasus)
 
-# Lendo data.frames auxiliares (criados em cria_dfs_auxiliares.R)
+
+# Lendo data.frames auxiliares (criados em cria_dfs_auxiliares.R) ---------
 df_cid10 <- read.csv("R/databases/df_cid10.csv") |>
   clean_names()
+
 df_aux_municipios <- read.csv("R/databases/df_aux_municipios.csv") |>
   clean_names() |>
   mutate_if(is.numeric, as.character)
 
-# Para os óbitos fetais: baixando os dados do SIM-DOFET de 2012 a 2022
+
+# Para os óbitos fetais ---------------------------------------------------
+## Baixando os dados do SIM-DOFET de 2012 a 2022
 df_sim_dofet_aux <- fetch_datasus(
   year_start = 2012,
   year_end = 2022,
@@ -38,37 +42,64 @@ df_fetais <- df_sim_dofet_aux |>
       peso >= 2000 & peso < 2500 ~ "2000 a 2499 g",
       peso >= 2500 ~ "\U2265 2500 g"
     ),
+    racacor = case_when(
+      racacor == "1" ~ "Branca",
+      racacor == "2" ~ "Preta",
+      racacor == "3" ~ "Amarela",
+      racacor == "4" ~ "Parda",
+      racacor == "5" ~ "Indígena",
+      is.na(racacor) | racacor == "9" ~ "Ignorado"
+    ),
+    grupo_principais = case_when(
+      causabas >= "P00" & causabas <= "P04" ~ "principais_p00_p04",
+      causabas >= "P05" & causabas <= "P08" ~ "principais_p05_p08",
+      causabas >= "P10" & causabas <= "P15" ~ "principais_p10_p15",
+      causabas >= "P20" & causabas <= "P29" ~ "principais_p20_p29",
+      causabas >= "P35" & causabas <= "P39" ~ "principais_p35_p39",
+      causabas >= "P50" & causabas <= "P61" ~ "principais_p50_p61",
+      causabas >= "P70" & causabas <= "P74" ~ "principais_p70_p74",
+      causabas >= "P75" & causabas <= "P78" ~ "principais_p75_p78",
+      causabas >= "P80" & causabas <= "P83" ~ "principais_p80_p83",
+      causabas >= "P90" & causabas <= "P96" ~ "principais_p90_p96",
+      causabas >= "Q00" & causabas <= "Q99" ~ "principais_q00_q99",
+      causabas >= "J00" & causabas <= "J99" ~ "principais_j00_j99",
+      causabas >= "A00" & causabas <= "A99" |
+        causabas >= "B00" & causabas <= "B99" ~ "principais_a00_b99"
+    ),
+    grupo_principais = ifelse(is.na(grupo_principais), "principais_outros", grupo_principais),
     obitos = 1
   ) |>
   left_join(df_aux_municipios) |>
   left_join(df_cid10) |>
-  select(codigo = codmunres, ano, municipio, uf, regiao, tipo_do_obito, capitulo_cid10, grupo_cid10, causabas_categoria, faixa_de_peso, obitos) |>
+  select(
+    codigo = codmunres, ano, municipio, uf, regiao, tipo_do_obito, causabas, 
+    capitulo_cid10, grupo_cid10, grupo_principais, causabas_categoria, faixa_de_peso,
+    racacor, obitos
+  ) |>
   group_by(across(!obitos)) |>
   summarise(obitos = sum(obitos)) |>
   ungroup()
 
-##Exportando os dados 
+## Exportando os dados 
 write.table(df_fetais, gzfile('R/databases/dados_obitos_fetais_2012_2022.csv.gz'), sep = ",", dec = ".", row.names = FALSE, fileEncoding = "utf-8")
 
 
-# Para os óbitos neonatais: baixando os dados do SIM de 2012 a 2022
-df_sim_aux <- fetch_datasus(
+# Para os óbitos neonatais ------------------------------------------------
+## Baixando os dados do SIM-DOINF de 2012 a 2022
+df_sim_doinf_aux <- fetch_datasus(
   year_start = 2012,
   year_end = 2022,
-  information_system = "SIM-DO"
+  information_system = "SIM-DOINF"
 ) |>
   clean_names()
 
 ## Criando a variável de ano, limitando a variável 'causabas' a três caracteres, filtrando pelos óbitos neonatais e pós-neonatais e criando algumas variáveis
-df_neonatais <- df_sim_aux |>
+df_neonatais <- df_sim_doinf_aux |>
   mutate(
     ano = as.numeric(substr(dtobito, nchar(dtobito) - 3, nchar(dtobito))),
     causabas = substr(causabas, 1, 3),
     peso = as.numeric(peso),
     idade = as.numeric(idade)
-  ) |>
-  filter(
-    idade <= 400
   ) |>
   mutate(
     tipo_do_obito = case_when(
@@ -83,11 +114,40 @@ df_neonatais <- df_sim_aux |>
       peso >= 2000 & peso < 2500 ~ "2000 a 2499 g",
       peso >= 2500 ~ "\U2265 2500 g"
     ),
+    racacor = case_when(
+      racacor == "1" ~ "Branca",
+      racacor == "2" ~ "Preta",
+      racacor == "3" ~ "Amarela",
+      racacor == "4" ~ "Parda",
+      racacor == "5" ~ "Indígena",
+      is.na(racacor) | racacor == "9" ~ "Ignorado"
+    ),
+    grupo_principais = case_when(
+      causabas >= "P00" & causabas <= "P04" ~ "principais_p00_p04",
+      causabas >= "P05" & causabas <= "P08" ~ "principais_p05_p08",
+      causabas >= "P10" & causabas <= "P15" ~ "principais_p10_p15",
+      causabas >= "P20" & causabas <= "P29" ~ "principais_p20_p29",
+      causabas >= "P35" & causabas <= "P39" ~ "principais_p35_p39",
+      causabas >= "P50" & causabas <= "P61" ~ "principais_p50_p61",
+      causabas >= "P70" & causabas <= "P74" ~ "principais_p70_p74",
+      causabas >= "P75" & causabas <= "P78" ~ "principais_p75_p78",
+      causabas >= "P80" & causabas <= "P83" ~ "principais_p80_p83",
+      causabas >= "P90" & causabas <= "P96" ~ "principais_p90_p96",
+      causabas >= "Q00" & causabas <= "Q99" ~ "principais_q00_q99",
+      causabas >= "J00" & causabas <= "J99" ~ "principais_j00_j99",
+      causabas >= "A00" & causabas <= "A99" |
+        causabas >= "B00" & causabas <= "B99" ~ "principais_a00_b99"
+    ),
+    grupo_principais = ifelse(is.na(grupo_principais), "principais_outros", grupo_principais),
     obitos = 1
   ) |>
   left_join(df_aux_municipios) |>
   left_join(df_cid10) |>
-  select(codigo = codmunres, ano, municipio, uf, regiao, tipo_do_obito, capitulo_cid10, grupo_cid10, causabas_categoria, faixa_de_peso, obitos) |>
+  select(
+    codigo = codmunres, ano, municipio, uf, regiao, tipo_do_obito, causabas,
+    grupo_principais, capitulo_cid10, grupo_cid10, causabas_categoria, faixa_de_peso, 
+    racacor, obitos
+  ) |>
   group_by(across(!obitos)) |>
   summarise(obitos = sum(obitos)) |>
   ungroup()
@@ -95,11 +155,7 @@ df_neonatais <- df_sim_aux |>
 ## Exportando os dados 
 write.table(df_neonatais, gzfile('R/databases/dados_obitos_neonatais_2012_2022.csv.gz'), sep = ",", dec = ".", row.names = FALSE, fileEncoding = "utf-8")
 
-# #Juntando as bases de óbitos fetais e neonatais
-# df_obitos_fetais_neonatais <- rbind(df_obitos_fetais, df_obitos_neonatais)
-# 
-# ##Exportando os dados 
-# write.table(df_obitos_fetais_neonatais, 'R/databases/dados_obitos_fetais_neonatais.csv', sep = ",", dec = ".", row.names = FALSE, fileEncoding = "utf-8")
+
 
 
 
--- R/baixa_dados_pt2.R
+++ R/baixa_dados_pt2.R
@@ -1,21 +1,37 @@
 library(tidyverse)
 library(janitor)
 library(readr)
+library(data.table)
 
-# Lendo data.frames auxiliares (criados em cria_dfs_auxiliares.R)
+# Lendo data.frames auxiliares (criados em cria_dfs_auxiliares.R) ---------
 df_cid10 <- read.csv("R/databases/df_cid10.csv") |>
   clean_names()
+
 df_aux_municipios <- read.csv("R/databases/df_aux_municipios.csv") |>
   clean_names() |>
   mutate_if(is.numeric, as.character)
 
-# Baixando os dados preliminares do SIM de 2023
-download.file("https://s3.sa-east-1.amazonaws.com/ckan.saude.gov.br/SIM/Mortalidade_Geral_2023.csv", "R/databases/DO23OPEN.csv", mode = "wb")
 
-df_sim_preliminares <- read.csv2("R/databases/DO23OPEN.csv") |>
+# Baixando os dados preliminares do SIM -----------------------------------
+download.file("https://s3.sa-east-1.amazonaws.com/ckan.saude.gov.br/SIM/DO23OPEN.csv", "R/databases/DO23OPEN.csv", mode = "wb")
+download.file("https://s3.sa-east-1.amazonaws.com/ckan.saude.gov.br/SIM/DO24OPEN+(2).csv", "R/databases/DO24OPEN.csv", mode = "wb")
+
+## Lendo os dados preliminares e excluindo os arquivos baixados
+df_sim_preliminares_2023 <- fread("R/databases/DO23OPEN.csv", sep = ";") |> 
   clean_names()
-
 file.remove("R/databases/DO23OPEN.csv")
+
+df_sim_preliminares_2024 <- fread("R/databases/DO24OPEN.csv", sep = ";") |> 
+  clean_names()
+file.remove("R/databases/DO24OPEN.csv")
+
+## Juntando os dados preliminares de 2023 e 2024
+df_sim_preliminares <- full_join(df_sim_preliminares_2023, df_sim_preliminares_2024)
+
+## Limpando a memória
+rm(df_sim_preliminares_2023, df_sim_preliminares_2024)
+gc()
+
 
 # Para os óbitos fetais ---------------------------------------------------
 ## Criando a variável de ano, limitando a variável 'causabas' a três caracteres e filtrando apenas pelos óbitos fetais que consideramos
@@ -27,7 +43,8 @@ df_fetais_preliminares <- df_sim_preliminares |>
     gestacao = as.character(gestacao),
     semagestac = as.numeric(semagestac),
     peso = as.numeric(peso),
-    codmunres = as.character(codmunres)
+    codmunres = as.character(codmunres),
+    racacor = as.character(racacor)
   ) |>
   filter(
     tipobito == "1",
@@ -42,11 +59,40 @@ df_fetais_preliminares <- df_sim_preliminares |>
       peso >= 2000 & peso < 2500 ~ "2000 a 2499 g",
       peso >= 2500 ~ "\U2265 2500 g"
     ),
+    racacor = case_when(
+      racacor == "1" ~ "Branca",
+      racacor == "2" ~ "Preta",
+      racacor == "3" ~ "Amarela",
+      racacor == "4" ~ "Parda",
+      racacor == "5" ~ "Indígena",
+      is.na(racacor) | racacor == "9" ~ "Ignorado"
+    ),
+    grupo_principais = case_when(
+      causabas >= "P00" & causabas <= "P04" ~ "principais_p00_p04",
+      causabas >= "P05" & causabas <= "P08" ~ "principais_p05_p08",
+      causabas >= "P10" & causabas <= "P15" ~ "principais_p10_p15",
+      causabas >= "P20" & causabas <= "P29" ~ "principais_p20_p29",
+      causabas >= "P35" & causabas <= "P39" ~ "principais_p35_p39",
+      causabas >= "P50" & causabas <= "P61" ~ "principais_p50_p61",
+      causabas >= "P70" & causabas <= "P74" ~ "principais_p70_p74",
+      causabas >= "P75" & causabas <= "P78" ~ "principais_p75_p78",
+      causabas >= "P80" & causabas <= "P83" ~ "principais_p80_p83",
+      causabas >= "P90" & causabas <= "P96" ~ "principais_p90_p96",
+      causabas >= "Q00" & causabas <= "Q99" ~ "principais_q00_q99",
+      causabas >= "J00" & causabas <= "J99" ~ "principais_j00_j99",
+      causabas >= "A00" & causabas <= "A99" |
+        causabas >= "B00" & causabas <= "B99" ~ "principais_a00_b99"
+    ),
+    grupo_principais = ifelse(is.na(grupo_principais), "principais_outros", grupo_principais),
     obitos = 1
   ) |>
   left_join(df_aux_municipios) |>
   left_join(df_cid10) |>
-  select(codigo = codmunres, ano, municipio, uf, regiao, tipo_do_obito, capitulo_cid10, grupo_cid10, causabas_categoria, faixa_de_peso, obitos) |>
+  select(
+    codigo = codmunres, ano, municipio, uf, regiao, tipo_do_obito, causabas,
+    grupo_principais, capitulo_cid10, grupo_cid10, causabas_categoria, faixa_de_peso,
+    racacor, obitos
+  ) |>
   group_by(across(!obitos)) |>
   summarise(obitos = sum(obitos)) |>
   ungroup()
@@ -54,14 +100,14 @@ df_fetais_preliminares <- df_sim_preliminares |>
 sum(df_fetais_preliminares$obitos)
 
 ## Juntando os dados preliminares com os dados de 2012 a 2021
-df_fetais_2022 <- read.csv(gzfile("R/databases/dados_obitos_fetais_2012_2022.csv.gz")) |>
+df_fetais_2012_2022 <- read.csv(gzfile("R/databases/dados_obitos_fetais_2012_2022.csv.gz")) |>
   mutate(codigo = as.character(codigo))
 
-df_fetais_completo <- full_join(df_fetais_2022, df_fetais_preliminares) |>
+df_fetais_completo <- full_join(df_fetais_2012_2022, df_fetais_preliminares) |>
   arrange(codigo, ano)
 
-##Exportando os dados 
-write.table(df_fetais_completo, gzfile('dados_oobr_obitos_fetais_2012_2023.csv.gz'), sep = ",", dec = ".", row.names = FALSE, fileEncoding = "utf-8")
+## Exportando os dados 
+write.table(df_fetais_completo, gzfile('dados_oobr_obitos_fetais_2012_2024.csv.gz'), sep = ",", dec = ".", row.names = FALSE, fileEncoding = "utf-8")
 
 
 # Para os óbitos neonatais ------------------------------------------------
@@ -72,7 +118,8 @@ df_neonatais_preliminares <- df_sim_preliminares |>
     codmunres = as.character(codmunres),
     causabas = substr(causabas, 1, 3),
     peso = as.numeric(peso),
-    idade = as.numeric(idade)
+    idade = as.numeric(idade),
+    racacor = as.character(racacor)
   ) |>
   filter(
     idade <= 400
@@ -90,24 +137,53 @@ df_neonatais_preliminares <- df_sim_preliminares |>
       peso >= 2000 & peso < 2500 ~ "2000 a 2499 g",
       peso >= 2500 ~ "\U2265 2500 g"
     ),
+    racacor = case_when(
+      racacor == "1" ~ "Branca",
+      racacor == "2" ~ "Preta",
+      racacor == "3" ~ "Amarela",
+      racacor == "4" ~ "Parda",
+      racacor == "5" ~ "Indígena",
+      is.na(racacor) | racacor == "9" ~ "Ignorado"
+    ),
+    grupo_principais = case_when(
+      causabas >= "P00" & causabas <= "P04" ~ "principais_p00_p04",
+      causabas >= "P05" & causabas <= "P08" ~ "principais_p05_p08",
+      causabas >= "P10" & causabas <= "P15" ~ "principais_p10_p15",
+      causabas >= "P20" & causabas <= "P29" ~ "principais_p20_p29",
+      causabas >= "P35" & causabas <= "P39" ~ "principais_p35_p39",
+      causabas >= "P50" & causabas <= "P61" ~ "principais_p50_p61",
+      causabas >= "P70" & causabas <= "P74" ~ "principais_p70_p74",
+      causabas >= "P75" & causabas <= "P78" ~ "principais_p75_p78",
+      causabas >= "P80" & causabas <= "P83" ~ "principais_p80_p83",
+      causabas >= "P90" & causabas <= "P96" ~ "principais_p90_p96",
+      causabas >= "Q00" & causabas <= "Q99" ~ "principais_q00_q99",
+      causabas >= "J00" & causabas <= "J99" ~ "principais_j00_j99",
+      causabas >= "A00" & causabas <= "A99" |
+        causabas >= "B00" & causabas <= "B99" ~ "principais_a00_b99"
+    ),
+    grupo_principais = ifelse(is.na(grupo_principais), "principais_outros", grupo_principais),
     obitos = 1
   ) |>
   left_join(df_aux_municipios) |>
   left_join(df_cid10) |>
-  select(codigo = codmunres, ano, municipio, uf, regiao, tipo_do_obito, capitulo_cid10, grupo_cid10, causabas_categoria, faixa_de_peso, obitos) |>
+  select(
+    codigo = codmunres, ano, municipio, uf, regiao, tipo_do_obito, causabas,
+    grupo_principais, capitulo_cid10, grupo_cid10, causabas_categoria, faixa_de_peso,
+    racacor, obitos
+  ) |>
   group_by(across(!obitos)) |>
   summarise(obitos = sum(obitos)) |>
   ungroup()
 
 ## Juntando os dados preliminares com os dados de 2012 a 2021
-df_neonatais_2022 <- read.csv(gzfile("R/databases/dados_obitos_neonatais_2012_2022.csv.gz")) |>
+df_neonatais_2012_2022 <- read.csv(gzfile("R/databases/dados_obitos_neonatais_2012_2022.csv.gz")) |>
   mutate(codigo = as.character(codigo))
 
-df_neonatais_completo <- full_join(df_neonatais_2022, df_neonatais_preliminares) |>
+df_neonatais_completo <- full_join(df_neonatais_2012_2022, df_neonatais_preliminares) |>
   arrange(codigo, ano)
 
-##Exportando os dados 
-write.table(df_neonatais_completo, gzfile('dados_oobr_obitos_neonatais_2012_2023.csv.gz'), sep = ",", dec = ".", row.names = FALSE, fileEncoding = "utf-8")
+## Exportando os dados 
+write.table(df_neonatais_completo, gzfile('dados_oobr_obitos_neonatais_2012_2024.csv.gz'), sep = ",", dec = ".", row.names = FALSE, fileEncoding = "utf-8")
 
 
 
